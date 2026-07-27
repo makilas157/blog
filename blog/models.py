397acfcdd9f4f269fe28ae9f2a30cf2e8ae3c764
Makila <makilas157@gmail.com>
2026-07-27 15:23:39 +0530

    initialized django blog and created homepage

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Category(models.Model):
+    category_name=models.CharField(max_length=50, unique=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural="categories"
+
+    def _str_(self):
+        return self.category_name
