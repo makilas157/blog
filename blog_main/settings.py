43d9236962cedd4fae7e39b808d3997f39deb8ae
Makila <makilas157@gmail.com>
2026-07-27 18:58:45 +0530

    initiailzed django blog and created homepage

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -115,8 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR /'static'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR /"blog__main" /"static"
 STATICFILES_DIRS = [
     'blog_main/static',
 ]
+STATIC_ROOT=BASE_DIR/"staticfiles"
+
+DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
+MEDIA_URL='/media/'
+MEDIA_ROOT=BASE_DIR/'media'
