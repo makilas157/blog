f48b2042888cfd315e353cfbf78a25fbef805b4e
Makila <makilas157@gmail.com>
2026-07-29 23:09:15 +0530

    Fixed Category model __str__ method and updated home page

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Category(models.Model):
-    category_name=models.CharField(max_length=50, unique=True)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
+    category_name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name_plural="categories"
+        verbose_name_plural = "categories"
 
-    def _str_(self):
+    def __str__(self):
         return self.category_name
 
 
 STATUS_CHOICES = (
     ("Draft", "Draft"),
-    ("Published","Published")
+    ("Published", "Published"),
 )
+
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=150, unique=True, blank=True)
@@ -26,10 +26,10 @@ class Blog(models.Model):
     featured_image = models.ImageField(upload_to='uploads/%y/%m/%d')
     short_description = models.TextField(max_length=500)
     blog_body = models.TextField(max_length=2000)
-    status = models.CharField(max_length=20,choices=STATUS_CHOICES,default="Draft")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Draft")
     is_featured = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def _str_(self):
+    def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Category, Blog
 
-# Create your views here.
+def home(request):
+    categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True,status="Published")
+    posts = Blog.objects.filter(is_featured=False,status="Published")
+    print(posts)
+    context = {
+        'categories': categories,
+        'featured_posts': featured_posts,
+        'posts': posts,
+    }
+
+    return render(request, 'home.html', context)
